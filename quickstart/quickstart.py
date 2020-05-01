5c9a9605123a02d4a7c261aded3d52a68abe5909
jyi <joshua.yi@amadeus.com>
2020-05-01 13:26:03 -0400

    Set cash

--- quickstart/quickstart.py
+++ quickstart/quickstart.py
@@ -5,9 +5,11 @@ import backtrader as bt
 
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
-
+    # Cerebro engine created broker instance in background
+    # instance already has cash to start with
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-
+    cerebro.broker.set_cash(100000.0)
+    # run() loops over data
     cerebro.run()
 
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
