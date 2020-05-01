6b20269a3db8dbbb62d578fe3c42afeff32feb38
jyi <joshua.yi@amadeus.com>
2020-05-01 13:37:22 -0400

    Add datas. Add data feed

--- quickstart/quickstart.py
+++ quickstart/quickstart.py
@@ -1,14 +1,37 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-
+import datetime
+import os.path  # Manage paths
+import sys  # Find out the script name (in argv[0
 import backtrader as bt
 
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
     # Cerebro engine created broker instance in background
+
+    # Datas are in a subfolder of the samples. Need to find where the script is
+    # because it could have been called from anywhere
+    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    datapath = os.path.join(modpath, 'datas/orcl-1995-2014.txt')
+
+    # Create a Data Feed
+    data = bt.feeds.YahooFinanceCSVData(
+        dataname=datapath,
+        # Do not pass values before this date
+        fromdate=datetime.datetime(2000, 1, 1),
+        # Do not pass values after this date
+        todate=datetime.datetime(2000, 12, 31),
+        reverse=False)
+
+    # Add Data Feed to Cerebro
+    cerebro.adddata(data)
+
+    # Set cash
+    cerebro.broker.setcash(100000.0)
+
     # instance already has cash to start with
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    cerebro.broker.set_cash(100000.0)
+
     # run() loops over data
     cerebro.run()
 
