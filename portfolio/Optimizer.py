5bd48b5d74a02ccb6cd22047603be5bbfcbd3ce9
Josh <jyi468@gmail.com>
2020-05-09 10:55:34 -0400

    Add tox. Add doc strings to optimizer

--- portfolio/Optimizer.py
+++ portfolio/Optimizer.py
@@ -1,9 +1,22 @@
+import pandas as pd
+
 class Optimizer:
     def __init__(self):
         pass
 
-    def train(self, data):
+    def train(self, x, y):
+        """
+        Train model with new data
+        :param x: Features
+        :param y: Labels
+        :return: None
+        """
         pass
 
-    def compute_portfolio(self):
-        pass
+    def compute_portfolio(self, x) -> pd.DataFrame:
+        """
+
+        :return: DataFrame of optimal portfolio allocation
+        Compute portfolio based on currently trained model
+        """
+        return pd.DataFrame()
