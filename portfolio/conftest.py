4b18b5e93a731d8bd8ca2ac60876d773e194865f
jyi <joshua.yi@amadeus.com>
2020-05-11 10:38:34 -0400

    Add test scaffolding

---
+++ portfolio/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+@pytest.fixture
+def sample_data():
+    # Parse / Get datas object
+    pass
---
+++ portfolio/optimizer_test.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+def train_accepts_correct_data_format():
+    pass
+
+
+def test_portfolio_has_symbols_and_allocations():
+    pass
