f3c6b4d4a72dc26c8d6a66eff7cd63cc80e36867
jyi <joshua.yi@amadeus.com>
2020-05-11 10:48:25 -0400

    Add passing test

--- portfolio/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.fixture
-def sample_data():
-    # Parse / Get datas object
-    pass
--- portfolio/optimizer_test.py
+++ portfolio/optimizer_test.py
@@ -1,8 +1,24 @@
 import pytest
+from Optimizer import Optimizer
 
 
-def train_accepts_correct_data_format():
+@pytest.fixture
+def x():
     pass
+
+
+@pytest.fixture
+def y():
+    pass
+
+
+@pytest.fixture
+def optimizer():
+    return Optimizer()
+
+
+def train_accepts_correct_data_format(optimizer, x, y):
+    assert optimizer.train(x, y) is None
 
 
 def test_portfolio_has_symbols_and_allocations():
